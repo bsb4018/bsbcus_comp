90fbb8fc2502ac34cf4e147020df3615b7af6a27
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-11-15 19:33:10 +0530

    Added Training Pipeline Code & Main File Code

---
+++ main.py
@@ -0,0 +1,44 @@
+
+import os,sys
+import argparse
+from finpred.logger import logger
+from finpred.exception import CustomerException
+from finpred.configuration.pipeline.training_config import FinanceConfig
+from finpred.pipeline.training import TrainingPipeline
+
+def start_training(start=False):
+    try:
+        if not start:
+            return None
+        print("Training Running")
+        TrainingPipeline(FinanceConfig()).start()
+        
+    except Exception as e:
+        raise CustomerException(e, sys)
+
+def start_prediction(start=False):
+    try:
+        pass
+    except Exception as e:
+        raise CustomerException(e, sys)
+
+
+def main(training_status, prediction_status):
+    try:
+        start_training(start=training_status)
+        start_prediction(start=prediction_status)
+    except Exception as e:
+        raise CustomerException(e, sys)
+
+if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--t", default=0, type=int, help="If provided true training will be done else not")
+        parser.add_argument("--p", default=0, type=int, help="If provided prediction will be done else not")
+        
+        args = parser.parse_args()
+
+        main(training_status=args.t, prediction_status=args.p)
+    except Exception as e:
+        print(e)
+        logger.exception(CustomerException(e, sys))
